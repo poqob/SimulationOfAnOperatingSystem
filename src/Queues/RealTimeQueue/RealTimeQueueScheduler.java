89c7e012bfcbaba3dbcb5b65452fe6b7a223f327
Mustafa <mmermmus123@gmail.com>
2023-12-26 01:10:36 +0300

    notes added for @OzturkVedat and @yetalit

--- src/Queues/RealTimeQueue/RealTimeQueueScheduler.java
+++ src/Queues/RealTimeQueue/RealTimeQueueScheduler.java
@@ -1,4 +1,5 @@
 package Queues.RealTimeQueue;
+
 import java.util.LinkedList;
 import java.util.concurrent.Semaphore;
 
@@ -7,18 +8,20 @@ import Process.Proces;
 import Hardware.*;
 
 public class RealTimeQueueScheduler {
+    RAM _ram;
     private final LinkedList<Proces> realTimeQueue;
-    
+
     public RealTimeQueueScheduler() {
+        _ram = RAM.getInstance();
         realTimeQueue = new LinkedList<>();
     }
-    
-    public void addProcess (Proces task) {
-    	task.ready();
-    	realTimeQueue.add(task);
+
+    public void addProcess(Proces task) {
+        task.ready();
+        realTimeQueue.add(task);
     }
-    
-    public void triggerScheduler (final Semaphore sem) {
+
+    public void triggerScheduler(final Semaphore sem) {
         if (!realTimeQueue.isEmpty()) {
             runQueue(realTimeQueue, sem);
             return;
@@ -26,36 +29,38 @@ public class RealTimeQueueScheduler {
         sem.release();
         System.out.println("RealTimeQueue Scheduler is Idle for this time interval!");
     }
-    
-    private void runQueue (LinkedList<Proces> fcfs, final Semaphore sem) {
-    	Proces task = fcfs.peek();	// Get the head
-		task.run();
-		// acquire needed resources here
-		sem.release();
-		// wait for one time interval
-	    try {
-	    	   Thread.sleep(10);
-	    } catch (InterruptedException e) {
-	    	   e.printStackTrace();
-	    }
-	    task.execute();
-    	if (task.getExecutionTime() > 0) {
-    	    fcfs.set(0,task);		// update the first process of the queue
-    	}
-    	else {
-			task=fcfs.pollFirst();		// discard the first process from the queue
-			RAM.getInstance().releaseMemory(task);
-			task.done();
-			FileOperations.doneProcessCount++;
-			//cpu.releaseProcess(task, 3);
-    	}
-    }
-    public void printStatus () {
-    	System.out.println("//------------------------------------------");
-    	System.out.print("RealTime: ");
-        for(Proces p : realTimeQueue) { 
-        	System.out.print(p.getPid() + "(" + p.getExecutionTime() + "), ");
+
+    // TODO: _ram.receiveMemory(task); // memmory allocation needed. Can you please allocate memory @OzturkVedat.
+    // NOTE: [Mustafa] i read run queue but my brain
+    private void runQueue(LinkedList<Proces> fcfs, final Semaphore sem) {
+        Proces task = fcfs.peek();    // Get the head
+        task.run();
+        // acquire needed resources here
+        sem.release();
+        // wait for one time interval
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-        System.out.println("\n--------------------------------------------");   
+        task.execute();
+        if (task.getExecutionTime() > 0) {
+            fcfs.set(0, task);        // update the first process of the queue
+        } else {
+            task = fcfs.pollFirst();        // discard the first process from the queue
+            RAM.getInstance().releaseMemory(task);
+            task.done();
+            FileOperations.doneProcessCount++;
+            //cpu.releaseProcess(task, 3);
+        }
+    }
+
+    public void printStatus() {
+        System.out.println("//------------------------------------------");
+        System.out.print("RealTime: ");
+        for (Proces p : realTimeQueue) {
+            System.out.print(p.getPid() + "(" + p.getExecutionTime() + "), ");
+        }
+        System.out.println("\n--------------------------------------------");
     }
 }
--- src/Queues/UserJobQueue/MultilevelFeedbackQueueScheduler.java
+++ src/Queues/UserJobQueue/MultilevelFeedbackQueueScheduler.java
@@ -1,11 +1,12 @@
 /*
  * It is important to note that a process that is in a lower priority queue
  * can only execute only when the higher priority queues are empty.
- * 
+ *
  * Any running process in the lower priority queue
  * can be interrupted by a process arriving in the higher priority queue.
  */
 package Queues.UserJobQueue;
+
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.concurrent.Semaphore;
@@ -18,25 +19,25 @@ public class MultilevelFeedbackQueueScheduler {
     private final int numberOfLevels;
     private final int[] timeQuantums;
     private final Queue<Proces>[] queues;
-    
+
     public MultilevelFeedbackQueueScheduler() {
         numberOfLevels = 3;
-        timeQuantums = new int[] { 1, 1, 1 };
+        timeQuantums = new int[]{1, 1, 1};
         queues = new LinkedList[numberOfLevels];
-        
+
         for (int i = 0; i < numberOfLevels; i++) {
             this.queues[i] = new LinkedList<>();
         }
     }
-    
-    public void addProcess (Proces task, int level) {
-    	task.ready();
-    	queues[level].add(task);
+
+    public void addProcess(Proces task, int level) {
+        task.ready();
+        queues[level].add(task);
     }
-    
+
     // This should be triggered by system timer on every interval (1 sec)
-    public void triggerScheduler (final Semaphore sem) {
-    	for (int i = 0; i < numberOfLevels; i++) {
+    public void triggerScheduler(final Semaphore sem) {
+        for (int i = 0; i < numberOfLevels; i++) {
             if (!queues[i].isEmpty()) {
                 runQueue(i, sem);
                 // A higher queue has tasks in it. So do not check the rest of the queues.
@@ -45,69 +46,68 @@ public class MultilevelFeedbackQueueScheduler {
         }
         System.out.println("[Yetalit]: My Scheduler is Idle for this interval!");
     }
-    
-    private void runQueue (int level, final Semaphore sem) {
-    	// Get the head
-    	Proces task = queues[level].poll();
-    	// check if ram is available
-    	if (RAM.getInstance().receiveMemory(task)) {
-    		task.run();
-    		try {
-        		// Wait for the realtime scheduler to acquire resources
-				sem.acquire();
-			} catch (InterruptedException e1) {
-				e1.printStackTrace();
-			}
-    		sem.release();
-    		// if needed sources are not available
+
+    private void runQueue(int level, final Semaphore sem) {
+        // Get the head
+        Proces task = queues[level].poll();
+        // check if ram is available
+        // TODO: Do we controll device availability here??? DeviceManager.getInstance().isThereEnoughDeviceSource(task) -> boolean
+        if (RAM.getInstance().receiveMemory(task)) {
+            task.run();
+            try {
+                // Wait for the realtime scheduler to acquire resources
+                sem.acquire();
+            } catch (InterruptedException e1) {
+                e1.printStackTrace();
+            }
+            sem.release();
+            // if needed sources are not available
     		/*
     		 *  // INTERRUPTED (2)
                 //cpu.releaseProcess(task, 2);
             	return;
     		 */
-    		// wait for the quantum of the current level
-	    	try {
-	    	    Thread.sleep(timeQuantums[level] * 10);
-	    	} catch (InterruptedException e) {
-	    	    e.printStackTrace();
-	    	}
+            // wait for the quantum of the current level
+            try {
+                Thread.sleep(timeQuantums[level] * 10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             task.execute();
-    		if (task.getExecutionTime() > 0) {
-    	    	if (level < 2) {
-    				level++;
-    			}
-    			// Add to the next queue
-    			addProcess(task, level);
-    		}
-    		else {
-    			RAM.getInstance().releaseMemory(task);
-    			task.done();
-				FileOperations.doneProcessCount++;
-    			// DONE (3)
-    			//cpu.releaseProcess(task, 3);
-    		}
-    	}
-    	else {
-    		task.interrupt();
-	    	if (level < 2) {
-				level++;
-			}
-			// Add to the next queue
-			addProcess(task, level);
-    		// INTERRUPTED (2)
-    		//cpu.releaseProcess(task, 2);
-    	}
-    }
-    
-    public void printStatus () {
-    	System.out.println("//------------------------------------------"); 
-        for (int i = 0; i < numberOfLevels; i++) {
-        	System.out.print(i + ": ");
-        	for(Proces p : queues[i]) { 
-        		System.out.print(p.getPid() + "(" + p.getExecutionTime() + "), ");
-        	}
-        	System.out.print('\n');
+            if (task.getExecutionTime() > 0) {
+                if (level < 2) {
+                    level++;
+                }
+                // Add to the next queue
+                addProcess(task, level);
+            } else {
+                RAM.getInstance().releaseMemory(task);
+                task.done();
+                FileOperations.doneProcessCount++;
+                // DONE (3)
+                //cpu.releaseProcess(task, 3);
+            }
+        } else {
+            task.interrupt();
+            if (level < 2) {
+                level++;
+            }
+            // Add to the next queue
+            addProcess(task, level);
+            // INTERRUPTED (2)
+            //cpu.releaseProcess(task, 2);
         }
-        System.out.println("--------------------------------------------"); 
+    }
+
+    public void printStatus() {
+        System.out.println("//------------------------------------------");
+        for (int i = 0; i < numberOfLevels; i++) {
+            System.out.print(i + ": ");
+            for (Proces p : queues[i]) {
+                System.out.print(p.getPid() + "(" + p.getExecutionTime() + "), ");
+            }
+            System.out.print('\n');
+        }
+        System.out.println("--------------------------------------------");
     }
 }
